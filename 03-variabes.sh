58b2a5d7b778580eb113bdbcc78f9eee7071ae14
Busapalli Nandiswar <nandiswarb93@gmail.com>
2025-11-03 17:24:33 +0530

    added variables

--- 03-variabes.sh
+++ 03-variabes.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 echo "this is third file"
+
+PERSON1=JOHN
+PERSON2=RAHEEM
+
+echo "$PERSON1: Hi $PERSON2, how are you today?"
+sleep 1
+
+echo "$PERSON2: Hey $PERSON1! I'm good, thanks. How about you?"
+sleep 1
+
+echo "$PERSON1: I'm doing well. Have you finished the project?"
+sleep 1
+
+echo "$PERSON2: Almost done. I just need to test a few things."
+sleep 1
+
+echo "$PERSON1: Great! Let me know if you need any help."
+sleep 1
+
+echo "$PERSON2: Sure, thanks!"
+sleep 1
+
+echo "Conversation ended."
