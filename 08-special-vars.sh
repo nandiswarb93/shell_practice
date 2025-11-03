da3bf6e5c4ffb563d7f353ae2779f750d127e0e3
Busapalli Nandiswar <nandiswarb93@gmail.com>
2025-11-03 20:22:24 +0530

    code added

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -7,3 +7,9 @@ echo $0
 echo "Current Directory : $PWD"
 echo "who is running this $USER"
 echo "home directory of user : $HOME"
+echo "Pid of this script : $$"
+
+sleep 50 &
+
+echo "PID if the last command in background is : $!"
+
