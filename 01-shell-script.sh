6d4164d8e52b5135364585bafdd8936179787cf2
Busapalli Nandiswar <nandiswarb93@gmail.com>
2025-11-03 17:45:54 +0530

    code added

--- 01-shell-script.sh
+++ 01-shell-script.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, how are you today?"
+sleep 1
+
+echo "$PERSON2: Hey $PERSON1! I'm good, thanks. How about you?"
+sleep 1
+
+echo "$PERSON1: I'm doing well. Have you finished the project?"
+sleep 1
+
+echo "$PERSON2: Almost done. I just need to test a few things."
+sleep 1
+
+echo "$PERSON1: Great! Let me know if you need any help."
+sleep 1
+
+echo "$PERSON2: Sure, thanks!"
+sleep 1
+
+echo "Conversation ended."
+
+
 echo "Helloworld"
