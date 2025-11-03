706224410af525cddaa0d74558049b5da2fbc729
Busapalli Nandiswar <nandiswarb93@gmail.com>
2025-11-03 20:15:32 +0530

    code added

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,3 +3,7 @@
 echo "all variables passed to the script: $@"
 echo "All variables passed to the script: $*"
 echo $0
+
+echo "Current Directory : $pwd"
+echo "who is running this $user"
+echo "home directory of user : $HOME"
