1c33235695fab7beb10cac24ad7082d4fffd1218
Busapalli Nandiswar <nandiswarb93@gmail.com>
2025-11-04 13:59:35 +0530

    if-else

--- 10-if-else.sh
+++ 10-if-else.sh
@@ -2,7 +2,7 @@
 #shebang
 NUMBER=10
 
-if [ $NUMBER -lt 10]; then
+if [ $NUMBER -lt 10 ]; then
     echo "Given number $NUMBER is lass than 10"
 else
     echo "given Number $NUMBER is grater than or equal to 10"
