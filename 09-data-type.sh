76764c3bc7240d5c50d34f1eff5a8f079bc0ac70
Busapalli Nandiswar <nandiswarb93@gmail.com>
2025-11-04 13:50:53 +0530

    if else

--- 09-data-type.sh
+++ 09-data-type.sh
@@ -6,4 +6,4 @@ NUMBER1=100
 NUMBER2=200
 
 SUM=$(($NUMBER1+$NUMBER2))
-echo  " sum of $NUMBER1 and $NUMBER2 is :  $SUM "
+echo  " sum of $NUMBER1 and $NUMBER2 is : $SUM "
---
+++ 10-if-else.sh
@@ -0,0 +1,12 @@
+#!/bin/bash //SHEBANG
+
+NUMBER=10
+
+if [ NUMBER -lt 10]; then
+    echo "Given number $NUMBER is lass than 10"
+else
+    echo "given Number $NUMBER is grater than or equal to 10"
+fi
+
+
+# -gt -eq -ne
