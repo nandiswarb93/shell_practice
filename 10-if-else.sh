ef4eb33b449203ba61950d318cfcd893b690aadd
Busapalli Nandiswar <nandiswarb93@gmail.com>
2025-11-04 14:05:19 +0530

    program

--- 10-if-else.sh
+++ 10-if-else.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #shebang
-NUMBER=10
+NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
     echo "Given number $NUMBER is lass than 10"
+elif [ $NUMBER -eq 10 ]; then
+    echo "given Number $NUMBER is equal to 10"
+elif [ $NUMBER -gt 10]; then
+    echo "given Number $NUMBER is grater than to 10"
 else
-    echo "given Number $NUMBER is grater than or equal to 10"
+    echo "none"
 fi
 
 
