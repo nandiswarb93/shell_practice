289e737d2e0eff00ff555187de569bcf2ae22297
Busapalli Nandiswar <nandiswarb93@gmail.com>
2025-11-04 16:27:17 +0530

    colors

--- 14-colors.sh
+++ 14-colors.sh
@@ -1,8 +1,51 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
 R="\e[31m"
 G="\e[32m"
 y="\e[33m"
 N="\e[0m"
 
-echo -e "$G Hello World"
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR :: Please run this script with root privelege"
+    exit 1 #failure is other than 0
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "Error :: Installing $2 .... is $R Failure $N"
+        exit 1
+    else
+        echo -e "$G Installing $2 is success $N"
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MYSQL"
+else
+    echo "MySql already exist ... $G Skipping $N"
+fi
+
+
+dnf installed nginx
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "Nginx"
+else
+    echo "Nginx already exist ... $G Skipping $N"
+fi
+
+
+
+
+dnf installed python3
+if [ $? -ne 0 ]; then
+    dnf install python3
+    VALIDATE $? "python3"
+else
+    echo "python3 already installed... $G Skipping $N"
+fi
